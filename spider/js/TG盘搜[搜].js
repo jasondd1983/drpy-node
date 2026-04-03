84a76fd12f411f4cddbbb4f9a228b9bef0d9cebb
jasondd1983 <xcming1983@gmail.com>
2026-04-03 12:55:42 +0800

    Update TG盘搜[搜].js

--- spider/js/TG盘搜[搜].js
+++ spider/js/TG盘搜[搜].js
@@ -10,8 +10,8 @@
 })
 */
 
-const Pan_API = 'https://pancheck.banye.tech:7777'; // PanCheck的API地址 自行替换
-const Pan_So = 'https://so.252035.xyz'; // PanSou地址，自行替换
+const Pan_API = 'http://sbb.20240211.xyz:6080'; // PanCheck的API地址 自行替换
+const Pan_So = 'https://psweb.zeabur.app'; // PanSou地址，自行替换
 const PAN_TYPES = '百度网盘,夸克网盘,UC网盘,天翼云盘,移动云盘'; // 默认搜索的网盘类型和排序，逗号分隔
 const MAX_RESULTS = 15; // 默认搜索结果数量 （每个网盘类型最多15条）
 const PAN_ORDER = 'desc'; // asc: 旧→新, desc: 新→旧（按时间排序）
